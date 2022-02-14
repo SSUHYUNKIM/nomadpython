d569d7658b042e24d34f4046c727712088e46cd2
수현 <43875264+SSUHYUNKIM@users.noreply.github.com>
2022-02-14 15:49:27 +0000

    #day1

---
+++ main.py
@@ -0,0 +1,32 @@
+#1.0
+# a_string = "like this"
+# a_number = 3
+# a_float = 3.12
+# a_boolean = False
+
+
+#1.1
+#days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+#print(days[3])
+#days.append("Sat")
+#print(days)
+
+#1.2
+# #nico = {
+#   "name": "Nico",
+#   "age": 29,
+#   "korean": True,
+#   "fav_food" :["Kimchi", "Bulgogi"]
+# }
+
+# print(nico)
+# nico["handsome"] = True
+# print(nico)
+
+#1.3
+age = "18"
+print(age)
+print(type(age))
+n_age = int(age)
+print(n_age)
+print(type(n_age))
